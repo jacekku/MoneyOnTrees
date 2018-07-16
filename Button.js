7cc7d6c47abcfa5b4f3a09cb282f53f847259f2a
jacekku <jacekku@github.com>
2018-07-16 19:12:12 +0200

    improved click handler and how the button is rendered

--- Button.js
+++ Button.js
@@ -2,19 +2,31 @@ class Button{
     constructor(x,y,width,height,onClick){
         this.x=x
         this.y=y
-        this.w=width
-        this.h=height
+        this.w=width||STYLE.buttonSize
+        this.h=height||STYLE.buttonSize
+        this.onClick=onClick
+        this.image
+    }
+    setOnClick(onClick){
         this.onClick=onClick
     }
+    setImage(image){
+        this.image=image
+    }
     show(){
-        fill(255,0,0)
-        rect(this.x,this.y,STYLE.buttonSize,STYLE.buttonSize)
+        fill(255)
+        rect(this.x,this.y,this.w,this.h)
+        image(this.image,this.x,this.y,this.w,this.h)
     }
     mouseIsInside(){
         return(mouseInsideRect(this.x,this.y,this.w,this.h))
     }
 
     isClicked(){
-        return(mouseInsideRect(this.x,this.y,this.w,this.h))
+        let output=mouseInsideRect(this.x,this.y,this.w,this.h)
+        if(output)
+            if(this.onClick)this.onClick()
+        return output
     }
+
 }

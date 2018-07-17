218f20bb4ca26e9d8f26fde6593eb21e7945f050
jacekku <jacekku@github.com>
2018-07-17 22:32:47 +0200

    refactoring how the orchard is rendered

--- Orchard.js
+++ Orchard.js
@@ -7,9 +7,9 @@ class Orchard {
         fill(255)
         rect(STYLE.orchard.x, STYLE.orchard.y, STYLE.orchard.width, STYLE.orchard.height)
         fill(100, 255, 100)
-        rect(STYLE.orchard.viewX, STYLE.orchard.viewY, STYLE.orchard.width, STYLE.orchard.height - STYLE.orchard.viewY + STYLE.margin)
+        rect(STYLE.orchard.x, STYLE.orchard.y+textSize(), STYLE.orchard.width, STYLE.orchard.height - textSize())
         fill(0)
-        text("ORCHARD", 10 + (600 / 2), textSize())
+        text("ORCHARD", STYLE.orchard.y+STYLE.orchard.width/2, textSize())
         for (const treeSpot of this.treeSpots) {
             treeSpot.show()
         }
@@ -20,9 +20,15 @@ class Orchard {
         }
     }
     fillTreeSpots() {
+        let startX=STYLE.orchard.x+STYLE.margin*1.5
+        let startY=STYLE.orchard.y+textSize()+STYLE.margin
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                this.treeSpots.push(new TreeSpot((2 * STYLE.margin) + (i * STYLE.treeSpotSize )+(i*STYLE.margin), STYLE.orchard.viewY+(STYLE.margin) + (j * STYLE.treeSpotSize)+(j*STYLE.margin)))
+                this.treeSpots.push(
+                new TreeSpot(
+                    startX+(j*(STYLE.treeSpotSize+STYLE.margin*2)),
+                    startY+(i*(STYLE.treeSpotSize+STYLE.margin))
+                ))
             }
         }
     }

eca747fddd41a4a4c8d8565973633ca784843a40
jacekku <jacekku@github.com>
2018-07-17 22:32:18 +0200

    added shop view and started item card showing

--- Shop.js
+++ Shop.js
@@ -4,9 +4,23 @@ class Shop{
         this.y=y
         this.w=w
         this.h=h
+        this.items=[new Item("Sapling",10,saplingImage,null,20,10)]
     }
     show(){
         fill(255)
         rect(this.x,this.y,this.w,this.h)
+        rect(this.x,this.y+textSize()+1,this.w,this.h-textSize()-1)
+        fill(0)
+        text("SHOP",this.x+this.w/2,textSize())
+
+        push()
+        
+        translate(this.x+STYLE.margin,this.y+STYLE.margin+textSize())
+        textSize(20)
+        for(const item of this.items){
+            item.showInShop(item.buyPrice,item.sellPrice,item.amount)
+            item.setShopButtons(x,y)
+        }
+        pop()
     }
 }

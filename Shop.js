541743914bcf1d9df5c3f6a469badb4e195fc096
jacekku <jacekku@github.com>
2018-07-18 23:22:02 +0200

    shop is now rendered a little bit better and more readable way

--- Shop.js
+++ Shop.js
@@ -4,7 +4,20 @@ class Shop{
         this.y=y
         this.w=w
         this.h=h
-        this.items=[new Item("Sapling",10,saplingImage,null,20,10)]
+        this.items=[
+            new Item("Sapling",0,saplingImage,null,15,2)
+            ,new Item("FILLER0",10,saplingImage,null,20,10)
+            ,new Item("FILLER1",10,saplingImage,null,20,10)
+            ,new Item("FILLER2",10,saplingImage,null,20,10)
+            ,new Item("FILLER3",10,saplingImage,null,20,10)
+            ,new Item("FILLER4",10,saplingImage,null,20,10)
+            ,new Item("FILLER5",10,saplingImage,null,20,10)
+            ,new Item("FILLER6",10,saplingImage,null,20,10)
+            ,new Item("FILLER7",10,saplingImage,null,20,10)
+            ,new Item("FILLER8",10,saplingImage,null,20,10)
+            ,new Item("FILLER9",10,saplingImage,null,20,10)
+            ,new Item("FILLER10",10,saplingImage,null,20,10)
+            ]
     }
     show(){
         fill(255)
@@ -12,15 +25,27 @@ class Shop{
         rect(this.x,this.y+textSize()+1,this.w,this.h-textSize()-1)
         fill(0)
         text("SHOP",this.x+this.w/2,textSize())
-
+        let tSize=STYLE.textSize
         push()
-        
-        translate(this.x+STYLE.margin,this.y+STYLE.margin+textSize())
         textSize(20)
+        imageMode(CENTER)
+        let index=0
         for(const item of this.items){
-            item.showInShop(item.buyPrice,item.sellPrice,item.amount)
+            let x=(index%4)
+            let y=((index-x)/4)
+            x=x+this.x+STYLE.margin + (x*(STYLE.treeSpotSize+STYLE.margin*5))
+            y=y+this.y+tSize+STYLE.margin + (y*(STYLE.treeSpotSize+STYLE.margin))
+
             item.setShopButtons(x,y)
+            
+            item.showInShop(x,y)
+            index++
         }
         pop()
+    }
+    isClicked(){
+        for(const item of this.items){
+            item.isClicked()
+        }
     }
 }

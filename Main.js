d4eaf1e1caef99e9880c5f9ba28a0a4347c775ff
jacekku <jacekku@github.com>
2018-07-19 22:39:22 +0200

    -moved functions to Functions.js -move tick to separate function for idle managing -buttons are now in an array for better managing

--- Main.js
+++ Main.js
@@ -2,91 +2,57 @@ let newTickEveryMS = 50
 let oldTickTime = Date.now()
 let running = true
 tickCounter = 0
-const STYLE = {
-    textSize: 30,
-    margin: 10,
-    buttonSize: 50,
-    buttonX: 10,
-    buttonY: 620,
-    treeSpotSize: undefined,
-    orchard: {
-        x: 10,
-        y: 10,
-        width: 600,
-        height: 600,
-        viewX: 10,
-        viewY: 10
-    },
-    inventory: {
-        x: 620,
-        y: 10,
-        width: 0,
-        height: 600,
-        viewOffsetX: 0,
-        viewOffsetY: 40
-    },
-    itemHeightInInventory: 50
-}
+
 let mouseObject
-let orchard, inventory, button, money, saplings, shop, shopButton
-let treeSheet, coinImage, saplingImage, shopImage
+let orchard, inventory,  money, oak_saplings, shop
+let buttons={
+    harvestButton:{},
+    shopButton:{}
+}
 let shopOpen=false
 let Actions
 function preload() {
-    treeSheet = loadImage('assets/TreeSheet.png')
-    saplingImage = loadImage('assets/TreeIcon.png')
-    coinImage = loadImage('assets/coin.png')
-    shopImage = loadImage('assets/shopIcon.png')
-    Actions = {
-        NOTHING: {
-            id: 0,
-            image: null
-        },
-        SELL: {
-            id: 1,
-            image: coinImage
-        },
-        PLANT: {
-            id: 2,
-            image: saplingImage
-        }
-    }
+    loadAllImages()
+    Actions=setupActions()
+    items=setupItems()
 }
 
 
 
 function setup() {
+
     createCanvas(900, 700)
     mouseObject = new Mouse()
-    money = new Item("Money", 10, coinImage, Actions.SELL)
-    saplings = new Item("Sapling", 5, saplingImage, Actions.PLANT)
     textAlign(CENTER, CENTER)
     textSize(STYLE.textSize)
     noCursor()
+    noStroke()
+   
     STYLE.buttonSize = height - (STYLE.orchard.height + 3 * STYLE.margin)
     STYLE.orchard.viewY += STYLE.textSize + STYLE.margin
     STYLE.treeSpotSize = (STYLE.orchard.height-textSize()-(STYLE.margin*4))/3
-    //(STYLE.orchard.height / 3) - (3 * STYLE.margin)
     STYLE.inventory.width = width - 620 - 10
-    orchard = new Orchard()
+    if(!orchard)orchard = new Orchard()
     inventory = new Inventory()
     shop = new Shop(10, 10, width - 20, height - 30 - STYLE.buttonSize)
-    inventory.items.push(money)
-    inventory.items.push(saplings)
+    inventory.items.push(items.money)
+    inventory.items.push(items.oak_saplings)
+    inventory.items.push(items.oak_log)
+    inventory.items.push(items.oak_plank)
 
 
 
-    button = new Button(STYLE.buttonX, STYLE.buttonY)
-    button.setImage(coinImage)
-    button.setOnClick(function () {
-        mouseObject.setAction(Actions.SELL)
+    buttons.harvestButton = new Button(STYLE.buttonX, STYLE.buttonY)
+    buttons.harvestButton.setImage(images.harvestImage)
+    buttons.harvestButton.setOnClick(function () {
+        mouseObject.setAction(Actions.HARVEST)
     })
-    shopButton = new Button(STYLE.buttonX + STYLE.margin + STYLE.buttonSize, STYLE.buttonY)
-    shopButton.setImage(shopImage)
-    shopButton.setOnClick(function () {
+    buttons.shopButton = new Button(STYLE.buttonX + STYLE.margin + STYLE.buttonSize, STYLE.buttonY)
+    buttons.shopButton.setImage(images.shopImage)
+    buttons.shopButton.setOnClick(function () {
         shopOpen = !shopOpen
     })
-
+    loadGame()
 }
 
 function draw() {
@@ -97,13 +63,12 @@ function draw() {
         orchard.show()
         inventory.show()
     }
-    button.show()
-    shopButton.show()
+    for(const button in buttons){
+        buttons[button].show()
+    }
     if (running && oldTickTime < Date.now()) {
-        orchard.tick()
-        inventory.tick()
+        tick()
         oldTickTime = Date.now() + newTickEveryMS;
-
     }
     fill(0)
     if (mouseObject.action && mouseObject.action.image) {
@@ -120,10 +85,15 @@ function mouseClicked() {
         orchard.isClicked()
         inventory.isClicked()
     }
-    button.isClicked()
-    shopButton.isClicked()
+    for(const button in buttons){
+        buttons[button].isClicked()
+    }
+}
+function tick(){
+    orchard.tick()
+    inventory.tick()
+    tickCounter++
 }
 
-document.onvisibilitychange = function () {
 
-}
+

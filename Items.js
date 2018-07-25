2cde2d195f1f592a5726aa7b9a2fa4eb4a049440
jacekku <jacekwyroba97@gmail.com>
2018-07-25 16:26:35 +0200

    added pine items

--- Items.js
+++ Items.js
@@ -3,9 +3,13 @@ let items = {}
 function setupItems() {
     let items = {
         money: new Item("Money", 10, images.coinImage),
-        oak_saplings: new Item("Oak Sapling", 5, images.oak_sapling, Actions.PLANT, 15, 2),
+        oak_saplings: new Item("Oak Sapling", 1, images.oak_sapling, Actions.PLANT, 15, 2,"oak"),
         oak_log: new Item("Oak Log", 0, images.oak_log, null, null, 20),
         oak_plank: new Item("Oak Plank", 0, images.oak_plank, null, null, 10),
+        pine_saplings: new Item("Pine Sapling", 1, images.pine_sapling, Actions.PLANT, 15, 2,"pine"),
+        pine_log: new Item("Pine Log", 0, images.pine_log, null, null, 20),
+        pine_plank: new Item("Pine Plank", 0, images.pine_plank, null, null, 10),
+
         iterator:{}
     }
     return items

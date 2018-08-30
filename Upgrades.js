0b0d569ca9849853f30ff66c2f2bf6eaa2d9348f
jacekku <jacekwyroba97@gmail.com>
2018-08-30 21:08:26 +0200

    creating and drawing cards are now two different functions

--- Upgrades.js
+++ Upgrades.js
@@ -8,33 +8,35 @@ class Upgrades{
             "harvestSpeed","chopSpeed","treeYield","fruitYield","sellPrice"
         ]
         this.upgradeCards=[]
+        this.setupUpgrades()
     }
-    drawUpgrades(){
+    setupUpgrades(){
         let cardSize=STYLE.itemInShopSize
         let upgradeIndex=0
         let y = this.y+(STYLE.margin)+textSize()
         let x = this.x+(STYLE.margin)
+        this.upgradeCards=[]
         for(let i=1;i<=12;i++){
                 if(upgradeIndex==-1)return
                 if(upgradeIndex!=-1){
                     let card=new Card(x,y,cardSize,cardSize)
+                    this.upgradeCards.push(card)
+                    card.name=this.availableUpgrades[upgradeIndex]
                     card.setSubSpaceShow("top", function(availableUpgrades,upgradeIndex){
                         push()
                         fill(0)
                         textSize(20)
-                        text(availableUpgrades[upgradeIndex],x+this.width/2,this.y+textSize())
+                        text(availableUpgrades[upgradeIndex],this.x+this.width/2,this.y+textSize())
                         pop()
                     },this.availableUpgrades,upgradeIndex)
                     card.setSubSpaceShow("bottom",function(stringToShow){
                         push()
                         fill(0)
                         textSize(20)
-                        text("current: "+stringToShow,x+this.width/2,this.y+textSize())
+                        text("current: "+stringToShow,this.x+this.width/2,this.y+textSize())
                         pop() 
                     },actionTimes[ this.availableUpgrades[upgradeIndex]].actualSpeed)
-                    card.showBackground()
-                    card.showSubSpace("top")
-                    card.showSubSpace("bottom")
+                    card.button=new Button(card.x,card.y,card.width,card.height,()=>{console.log(card.name)})
                 }
 
                 x+=cardSize+STYLE.margin*5
@@ -48,14 +50,25 @@ class Upgrades{
             }
     }
 
-
+    drawUpgrades(){
+        for(let card in this.upgradeCards){
+            card=this.upgradeCards[card]
+            card.showBackground()
+            card.showAllSubSpaces()
+        }
+    }
 
     show(){
         drawView(this.x, this.y, this.width, this.height,"UPGRADES (work in progress)")
         this.drawUpgrades()
-        
-        
-        
+    }
+    isClicked() {
+        for (let card in this.upgradeCards) {
+            card = this.upgradeCards[card]
+            if (card.button) {
+                if(card.button.isClicked())this.setupUpgrades()
+            }
+        }
     }
 
     
